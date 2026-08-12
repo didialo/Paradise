26924f582d55841ef2e50f6b63b98e172bf347d9
didialo <78569796+didialo@users.noreply.github.com>
2026-08-12 16:17:18 -0400

    Add files via upload

--- src/deploy-commands.js
+++ src/deploy-commands.js
@@ -26,18 +26,18 @@ const rest = new REST({ version: '10' })
     .setToken(process.env.DISCORD_TOKEN);
 
 async function deploy() {
+    
     try {
         console.log(`Registering ${commands.length} commands...`);
 
         await rest.put(
-            Routes.applicationGuildCommands(
-                process.env.CLIENT_ID,
-                process.env.GUILD_ID
-            ),
-            {
-                body: commands
-            }
-        );
+    Routes.applicationCommands(
+        process.env.CLIENT_ID
+    ),
+    {
+        body: commands
+    }
+);
 
         console.log('All Dude commands registered in Paradise.');
     } catch (error) {
